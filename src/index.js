f53238973bb51c8db2b47a63bfd6bdbf4c06a266
Esioan <esioan667@gmail.com>
2021-06-23 21:34:28 +0100

    Add new review support

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
+import AddMovieReviewPage from './pages/addMovieReviewPage';
 import SiteHeader from './components/siteHeader';
 import UpcomingMoviesPage from './pages/upcomingMoviesPage';
 import { QueryClientProvider, QueryClient } from "react-query";
@@ -30,6 +31,7 @@ const App = () => {
           {" "}
           <Switch>
             <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
+            <Route exact path="/reviews/form" component={AddMovieReviewPage} />
             <Route path="/reviews/:id" component={MovieReviewPage} />
             <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
             <Route path="/movies/:id" component={MoviePage} />
