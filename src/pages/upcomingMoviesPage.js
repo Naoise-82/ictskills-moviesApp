79bb3d87a75e42ead0d6f2be601fe090618aeed6
Esioan <esioan667@gmail.com>
2021-06-23 22:03:45 +0100

    Fix upcoming movies page

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PageTemplate from '../components/templateMovieListPage';
 import { getUpcomingMovies } from "../api/tmdb-api";
-import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
 import PlaylistAddIcon from "@material-ui/icons/PlaylistAdd";
